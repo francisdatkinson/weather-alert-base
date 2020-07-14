aaec40ee0bc8a5f6f8afef63cf11f98269f93ead
TakeThatkinson <francisdatkinson@gmail.com>
2020-07-14 15:01:22 +0100

    Header/form stlyes
    Styled header and location input
    
    Added logo and favicon
    
    Added wind direction icon with windspeed in centre
    
    Added sentinel data to test location layout

--- src/components/LocationItemSentinel.tsx
+++ src/components/LocationItemSentinel.tsx
@@ -28,8 +28,8 @@ class LocationItemSentinel extends LocationItem {
     // return location name and error message
     return (
       <div className="sentinel">
-        <h2>{item.name}</h2>
-        <h3>Monday 17:00</h3>
+        <h3>{item.name}</h3>
+        <h4>Monday 17:00</h4>
         <div
           className="button remove"
           color="secondary"
